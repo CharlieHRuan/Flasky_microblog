8299eca24cec7e9eeae2e3859f9c28a7a9383de6
WuHaaaaa <ruanheng159@163.com>
2019-03-08 13:11:11 +0800

    添加Web程序报错发送邮件、记录本地日志功能

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,9 @@
+# 导入日志包
+import logging
+# 启用邮件记录器，本地基于文件的日志记录器
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+
 from flask import Flask
 # 从config模块中导入Config类
 from config import Config
@@ -22,3 +28,42 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 from app import routes, models, errors
+
+# 仅当应用的DEBUG模式未开启的时候，我们才执行发送邮件
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='MicroBlog Failure',
+            credentials=auth, secure=secure)
+        # 设置级别为严重级别，只报告错误信息
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+        # 创建一个日志文件夹保存日志文件
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        # 确保创建文件单个大小在10kb，并只保留最后10个文件作为备份
+        file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, 
+                                            backupCount=10)
+        # 设置日志文件格式
+        # 时间戳、错误级别、信息、日志来源的源代码文件、行号
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        # 设置日志级别，什么等级的信息，才记录在日志文件中，这里选用INFO级别
+        # 级别递增
+        # DEBUG、INFO、WARNING、ERROR、CRITICAL
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        # 服务器每次启动的时候，都会写入一行表示服务器启动
+        app.logger.info('Microblog startup')
+
--- config.py
+++ config.py
@@ -11,3 +11,16 @@ class Config(object):
     # 设置数据发生变更之后是否发送信号给应用
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    # 设置邮件服务器配置
+    # 服务器地址
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    # 端口号
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    # 服务器凭证默认不启用
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    # 用户名
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    # 密码
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    # 收到错误邮件的地址
+    ADMINS = ['ruanheng159@163.com']
